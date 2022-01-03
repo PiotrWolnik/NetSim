5bc43df395290602f32e8a8bc480b8e2b364b16c
PiotrWolnik <80884058+PiotrWolnik@users.noreply.github.com>
2022-01-03 11:25:25 +0100

    Pierwsze uzupełnienie metod w pliku nodes.cpp

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -2,3 +2,28 @@
 // Created by peter on 19.12.2021.
 //
 
+#include "nodes.hpp"
+
+ReceiverPreferences::const_iterator ReceiverPreferences::begin() const {
+    return preferences.begin();
+}
+
+ReceiverPreferences::const_iterator ReceiverPreferences::cbegin() const {
+    return preferences.cbegin();
+}
+
+ReceiverPreferences::const_iterator ReceiverPreferences::end() const {
+    return preferences.end();
+}
+
+ReceiverPreferences::const_iterator ReceiverPreferences::cend() const {
+    return preferences.cend();
+}
+
+void Worker::receive_package(Package&& p) {
+    PackageSender::push_package(std::move(p));
+}
+
+void Storehouse::receive_package(Package&& p) {
+    storehouse_->push(std::move(p));
+}
