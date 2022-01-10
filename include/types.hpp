5d9fbd8eca0b9c652b0dad0e9273dc383f7d94d3
PiotrWolnik <80884058+PiotrWolnik@users.noreply.github.com>
2022-01-10 16:01:26 +0100

    Uzupełnienie pliku types.hpp o kolejne typy

--- include/types.hpp
+++ include/types.hpp
@@ -1,12 +1,31 @@
 //
 // Created by peter on 19.12.2021.
 //
-
+//Członkowie grupy:
+//(1) Piotr Wolnik, 403077
+//(2) Kamil Kosakowski, 405035
+//(3) Mikołaj Kapera, 405208
+//
+//Kierownik grupy: Piotr Wolnik
+//
+//URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
 #ifndef SIECI_TYPES_HPP
 #define SIECI_TYPES_HPP
 
 #include <set>
+#include <functional>
 
 using ElementID = unsigned int;
+using Time = int;
+using TimeOffset = int;
+using ProbabilityGenerator = std::function<double()>;
 
 #endif //SIECI_TYPES_HPP
+//Członkowie grupy:
+//(1) Piotr Wolnik, 403077
+//(2) Kamil Kosakowski, 405035
+//(3) Mikołaj Kapera, 405208
+//
+//Kierownik grupy: Piotr Wolnik
+//
+//URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
