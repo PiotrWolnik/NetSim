d2d1c434dcd42c1e715df6ef292b75f2436c513b
PiotrWolnik <80884058+PiotrWolnik@users.noreply.github.com>
2022-05-12 09:50:38 -0800

    Update factory.hpp

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,13 +1,7 @@
 //
 // Created by peter on 19.12.2021.
 //
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
 #ifndef SIECI_FACTORY_HPP
 #define SIECI_FACTORY_HPP
@@ -140,11 +134,5 @@ void Factory::remove_receiver(NodeCollection<Node>& collection, ElementID id) {
 }
 
 #endif //SIECI_FACTORY_HPP
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
