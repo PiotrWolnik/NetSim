bce79a299e3bb3fcab7d71baf7e08b89280de46f
PiotrWolnik <80884058+PiotrWolnik@users.noreply.github.com>
2022-05-12 09:49:06 -0800

    Update reports (1).cpp

--- src/reports (1).cpp
+++ src/reports (1).cpp
@@ -1,13 +1,6 @@
 //
 // Created by peter on 19.12.2021.
-//
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
 #include <reports.hpp>
 
@@ -120,11 +113,5 @@ bool SpecificTurnsReportNotifier::should_generate_report(Time t) {
     }
     return false;
 }
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
