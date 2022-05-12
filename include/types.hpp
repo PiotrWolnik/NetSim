e6ade593bcaa8d4c4d8d144ab640bd843964f9d1
PiotrWolnik <80884058+PiotrWolnik@users.noreply.github.com>
2022-05-12 09:52:10 -0800

    Update types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -1,13 +1,6 @@
 //
 // Created by peter on 19.12.2021.
 //
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
 #ifndef SIECI_TYPES_HPP
 #define SIECI_TYPES_HPP
@@ -21,11 +14,5 @@ using TimeOffset = int;
 using ProbabilityGenerator = std::function<double()>;
 
 #endif //SIECI_TYPES_HPP
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
