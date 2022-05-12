5277cc6ce1cd766c0c57cf84975005c992199ef0
PiotrWolnik <80884058+PiotrWolnik@users.noreply.github.com>
2022-05-12 09:51:51 -0800

    Update simulation.hpp

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -1,13 +1,7 @@
 //
 // Created by peter on 19.12.2021.
 //
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
 #ifndef SIECI_SIMULATION_HPP
 #define SIECI_SIMULATION_HPP
@@ -16,11 +10,5 @@
 extern void simulate(Factory & f, TimeOffset d, std::function<void(Factory&, Time)> rf);
 
 #endif //SIECI_SIMULATION_HPP
-//Członkowie grupy:
-//(1) Piotr Wolnik, 403077
-//(2) Kamil Kosakowski, 405035
-//(3) Mikołaj Kapera, 405208
-//
-//Kierownik grupy: Piotr Wolnik
-//
+
 //URL repozytorium: https://github.com/PiotrWolnik/NetSim.git
